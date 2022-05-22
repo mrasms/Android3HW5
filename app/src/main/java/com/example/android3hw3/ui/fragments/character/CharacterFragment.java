918e4e3e4cd1b37d44f14d7981a29009a136d331
mrasms <shurik96kg@gmail.com>
2022-05-23 01:09:50 +0600

    настроил пагинацию согласно инструкции с урока, убрал ошибку дублирования списка при переключении фрагментов введение одного булеана в жизненный цикл.

--- app/src/main/java/com/example/android3hw3/ui/fragments/character/CharacterFragment.java
+++ app/src/main/java/com/example/android3hw3/ui/fragments/character/CharacterFragment.java
@@ -1,5 +1,8 @@
 package com.example.android3hw3.ui.fragments.character;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,16 +14,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.android3hw3.base.BaseFragment;
-import com.example.android3hw3.data.repositories.CharacterRepository;
 import com.example.android3hw3.databinding.FragmentCharacterBinding;
 import com.example.android3hw3.models.CharacterModel;
 import com.example.android3hw3.models.RickAndMortyResponse;
 import com.example.android3hw3.ui.adapters.CharacterAdapter;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class CharacterFragment extends BaseFragment<FragmentCharacterBinding> {
 
@@ -43,7 +47,10 @@ public class CharacterFragment extends BaseFragment<FragmentCharacterBinding> {
         linearLayoutManager = new LinearLayoutManager(requireContext());
         binding.characterRecView.setLayoutManager(linearLayoutManager);
         binding.characterRecView.setAdapter(characterAdapter);
-        binding.characterRecView.onScrolled(0,0);
+    }
+
+    @Override
+    protected void setupListener() {
         binding.characterRecView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
@@ -52,52 +59,13 @@ public class CharacterFragment extends BaseFragment<FragmentCharacterBinding> {
                     visibleCount = linearLayoutManager.getChildCount();
                     totalCount = linearLayoutManager.getItemCount();
                     postVisible = linearLayoutManager.findFirstVisibleItemPosition();
-                    if (loading) {
-                        linearLayoutManager.setStackFromEnd(false);
-
-                        if ((visibleCount + postVisible) >= totalCount) {
-                            linearLayoutManager.setStackFromEnd(false);
-
-                            loading = false;
-
+                    if ((visibleCount + postVisible) >= totalCount) {
+                        if (characterViewModel.characterPage != totalCount && (characterViewModel.characterPage < totalCount)) {
                             characterViewModel.characterPage++;
-                            characterViewModel.getList().observe(getViewLifecycleOwner(), new Observer<RickAndMortyResponse<CharacterModel>>() {
-                                @Override
-                                public void onChanged(RickAndMortyResponse<CharacterModel> characterModelRickAndMortyResponse) {
-                                    characterAdapter.submitList(characterModelRickAndMortyResponse.getResults());
-                                    loading=true;
+                            if (!loading && (characterViewModel.characterPage < totalCount)) {
 
-                                }
-                            });
-                       }
-                    }
-                }
-            }
-        });
-        binding.characterRecView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                if (dy < 0) {
-                    visibleCount = linearLayoutManager.getChildCount();
-                    totalCount = linearLayoutManager.getItemCount();
-                    postVisible = linearLayoutManager.findLastVisibleItemPosition();
-                    if (loading) {
-                        linearLayoutManager.setStackFromEnd(true);
-                        if ((visibleCount + postVisible) >= totalCount) {
-                            linearLayoutManager.setStackFromEnd(true);
-                            if (totalCount-visibleCount==postVisible){
-                            loading = false;
-                            characterViewModel.characterPage--;
-                            characterViewModel.getList().observe(getViewLifecycleOwner(), new Observer<RickAndMortyResponse<CharacterModel>>() {
-
-                                @Override
-                                public void onChanged(RickAndMortyResponse<CharacterModel> characterModelRickAndMortyResponse) {
-                                    characterAdapter.submitList(characterModelRickAndMortyResponse.getResults());
-                                    characterAdapter.submitList(characterModelRickAndMortyResponse.getResults());
-                                    loading=true;
-                                }
-                            });}
+                                fetchCharacters();
+                            }
                         }
                     }
                 }
@@ -105,13 +73,64 @@ public class CharacterFragment extends BaseFragment<FragmentCharacterBinding> {
         });
     }
 
+    private void fetchCharacters() {
+        if (isNetwork()) {
+            characterViewModel.getList().observe(getViewLifecycleOwner(), new Observer<RickAndMortyResponse<CharacterModel>>() {
+                @Override
+                public void onChanged(RickAndMortyResponse<CharacterModel> characterModelRickAndMortyResponse) {
+                    if (!loading) {
+                        ArrayList<CharacterModel> list = new ArrayList<>(characterAdapter.getCurrentList());
+                        list.addAll(characterModelRickAndMortyResponse.getResults());
+                        characterAdapter.submitList(list);
+                        if (list != characterAdapter.getCurrentList()) {
+                            Toast.makeText(requireContext(), "totalCount" + totalCount, Toast.LENGTH_SHORT).show();
+                            characterAdapter.submitList(list);
+                        }
+                    }
+
+                        /*list.removeAll(characterModelRickAndMortyResponse.getResults());
+
+                        characterAdapter.submitList(list);*/
+
+                }
+            });
+        } else
+            characterAdapter.submitList((List<CharacterModel>) characterViewModel.getList());
+
+    }
+
+    private boolean isNetwork() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
     @Override
     protected void setupRequest() {
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
         characterViewModel.getList().observe(getViewLifecycleOwner(), new Observer<RickAndMortyResponse<CharacterModel>>() {
             @Override
             public void onChanged(RickAndMortyResponse<CharacterModel> characterModelRickAndMortyResponse) {
-                characterAdapter.submitList(characterModelRickAndMortyResponse.getResults());
+                if (loading) {
+                    ArrayList<CharacterModel> list = new ArrayList<>(characterAdapter.getCurrentList());
+                    list.addAll(characterModelRickAndMortyResponse.getResults());
+                    Toast.makeText(requireContext(), "totalCount" + totalCount, Toast.LENGTH_SHORT).show();
+
+                    characterAdapter.submitList(list);
+                    loading = false;
+                }
             }
         });
+
     }
- }
+}
