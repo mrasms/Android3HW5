1ee4075966c982e89fa5b5aff77715dd0a9dff8f
mrasms <shurik96kg@gmail.com>
2022-05-27 23:15:44 +0600

    initial commit

---
+++ app/src/main/java/com/example/android3hw3/ui/adapters/clickers/OnLocationItemClick.java
@@ -0,0 +1,10 @@
+package com.example.android3hw3.ui.adapters.clickers;
+
+import com.example.android3hw3.models.EpisodeModel;
+import com.example.android3hw3.models.LocationModel;
+
+public interface OnLocationItemClick {
+
+    void onItemClick(LocationModel model);
+
+}
